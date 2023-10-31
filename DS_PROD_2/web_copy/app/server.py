18eecd3c953b1f3fcbb349e06873b1512792b692
Evgeniy HP <eko867@yandex.ru>
2023-10-31 08:28:52 +0300

    ds_prod_2

---
+++ DS_PROD_2/web_copy/app/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+with open('/app/models/model.pkl', 'rb') as pkl_file:
+    model = pickle.load(pkl_file)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    msg = "Тестовое сообщение. Сервер запущен!"
+    return msg
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = np.array(request.json)
+    features = features.reshape(1, 4)
+    prediction = model.predict(features)
+    return  jsonify({'prediction': prediction[0]})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
